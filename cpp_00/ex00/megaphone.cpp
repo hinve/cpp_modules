189ed578986c8f8319a81f59829fc8b18a8eb7eb
Héctor Pino <140248223+hinve@users.noreply.github.com>
2024-12-16 18:50:35 +0000

    cambios

--- cpp_00/ex00/megaphone.cpp
+++ cpp_00/ex00/megaphone.cpp
@@ -6,24 +6,28 @@ void print(string str) {
     cout << str << endl;
 }
 
-void toUpper(const string& str, string& outputString) {
+void strToUpper(const string& str, string& outputString) {
     for(size_t i = 0; i < str.size(); i++) {
         outputString += toupper(str[i]);
     }
+}
+
+void megaphone(int argc, char *argv[]) {
+    stringstream ss; 
+
+    for(int i = 1; i < argc; ++i) {
+        ss << argv[i];
+        ss << " ";
+    }
+    string outputString;
+    strToUpper(ss.str(), outputString);
+    print(outputString);
 }
 
 int main(int argc, char *argv[]) {
     if(argc == 1) {
         print("* LOUD AND UNBEARABLE FEEDBACK NOISE *");
     } else {
-        stringstream ss; 
-
-        for(int i = 1; i < argc; ++i) {
-            ss << argv[i];
-            ss << " ";
-        }
-        string outputString;
-        toUpper(ss.str(), outputString);
-        print(outputString);
+        megaphone(argc, argv);
     }
 }
